3f60ff148144ca4e66e20f4836a7c3835a8c2024
Radosław Kujawa <radoslaw.kujawa@c0ff33.net>
2016-01-18 14:32:31 +0100

    Refactor to use God object instead of globals 😰.

--- src/akutest.c
+++ src/akutest.c
@@ -1,6 +1,3 @@
-/* note: this is overly complex */
-/* note2: globals, globals everywhere */
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -11,6 +8,8 @@
 #include <exec/interrupts.h>
 
 #include <hardware/intbits.h>
+
+#define DEBUG			1
 
 #define CLOCKPORT_BASE		0xD80001
 #define CLOCKPORT_STRIDE	4
@@ -48,128 +47,146 @@ typedef enum {
 	OP_READ 
 } op_t;
 
-UBYTE clockport_read(UBYTE reg);
-void clockport_write(UBYTE reg, UBYTE val);
-void pca9564_isr(void);
-void pca9564_dump_state(void);
-void pca9564_send_start(void);
-void pca9564_send_stop(void);
-void pca9564_read_1(UBYTE);
+/* glorious god object that holds the state of everything in this program; tldr */
+typedef struct {
+	op_t cur_op; 
 
-static UBYTE *cp = CLOCKPORT_BASE;
+	UBYTE *cp;
 
-static UBYTE slave_addr = 0; 
+	BYTE sig_intr;
+	LONG sigmask_intr;
+	struct Task *MainTask;
 
-static BYTE sig_intr = -1;
-static LONG sigmask_intr;
-static struct Task *MainTask = NULL;
+	UBYTE slave_addr; 
+	UBYTE bytes_left;
+#ifdef DEBUG
+	int isr_called; /* how may times ISR was called */
+	BOOL in_isr;
+#endif /* DEBUG */
+} pca9564_state_t;
 
-static op_t cur_op = OP_NOP;
-static UBYTE bytes_left = 0;
-
-static const BOOL debug = TRUE;
-static int isr_called = 0; /* how may times ISR was called */
+UBYTE clockport_read(pca9564_state_t *, UBYTE);
+void clockport_write(pca9564_state_t *, UBYTE, UBYTE);
+__amigainterrupt void pca9564_isr(pca9564_state_t *);
+void pca9564_dump_state(pca9564_state_t *);
+void pca9564_send_start(pca9564_state_t *);
+/*void pca9564_send_stop(void); XXX */
+void pca9564_read_1(pca9564_state_t *, UBYTE);
 
 UBYTE
-clockport_read(UBYTE reg) 
+clockport_read(pca9564_state_t *sp, UBYTE reg) 
 {
 	UBYTE v;
 	UBYTE *ptr;
 
-	ptr = cp + (reg * CLOCKPORT_STRIDE);
+	ptr = sp->cp + (reg * CLOCKPORT_STRIDE);
 	v = *ptr;
-	if (debug)
+#ifdef DEBUG 
+	if (!(sp->in_isr))
 		printf("DEBUG: read %x from %p\n", (int) v, (void*) ptr);
+#endif /* DEBUG */
 
 	return v;
 }
 
 void
-clockport_write(UBYTE reg, UBYTE value)
+clockport_write(pca9564_state_t *sp, UBYTE reg, UBYTE value)
 {
 	UBYTE *ptr;
 
-	ptr = cp + (reg * CLOCKPORT_STRIDE);
-	if (debug)
+	ptr = (sp->cp) + (reg * CLOCKPORT_STRIDE);
+#ifdef DEBUG 
+	if (!(sp->in_isr))
 		printf("DEBUG: write %x to %p\n", (int) value, (void*) ptr);
+#endif /* DEBUG */
+
 	*ptr = value;
 }
 
 int main(void)
 {
+	pca9564_state_t sc;
 	struct Interrupt *int6;
-	UBYTE ctrl;
-	UBYTE void_data;
 
-	if ((sig_intr = AllocSignal(-1)) == -1) {
+	UBYTE ctrl;
+
+#ifdef DEBUG
+	sc.in_isr = FALSE;
+	sc.isr_called = 0;
+#endif /* DEBUG */
+	sc.cp = CLOCKPORT_BASE;
+	sc.cur_op = OP_NOP;
+
+	sc.sig_intr = -1;
+	if ((sc.sig_intr = AllocSignal(-1)) == -1) {
 		printf("Couldn't allocate signal\n");
 		return 1;
 	}
-	sigmask_intr = 1L << sig_intr;
+	sc.sigmask_intr = 1L << sc.sig_intr;
 
-	MainTask = FindTask(NULL);
+	sc.MainTask = FindTask(NULL);
 
 	if (int6 = AllocMem(sizeof(struct Interrupt), MEMF_PUBLIC|MEMF_CLEAR)) {
 		int6->is_Node.ln_Type = NT_INTERRUPT;
 		int6->is_Node.ln_Pri = -60;
 		int6->is_Node.ln_Name = "PCA9564";
-		int6->is_Data = (APTR)&void_data;
+		int6->is_Data = (APTR)&sc;
 		int6->is_Code = pca9564_isr;
 
 		AddIntServer(INTB_EXTER, int6); 
 	} else {
 		printf("Can't allocate memory for interrupt node\n");
-		FreeSignal(sig_intr);
+		FreeSignal(sc.sig_intr);
 		return 1;
 	}
 
 	/* init the host controller */
 	ctrl = I2CCON_CR_59KHZ | I2CCON_ENSIO;
-	clockport_write(I2CCON, ctrl);
+	clockport_write(&sc, I2CCON, ctrl);
 	Delay(50);
 
-	pca9564_read_1(0x48); 	/* XXX */
+	pca9564_read_1(&sc, 0x48); 	/* XXX */
 
 	ctrl = 0;
-	clockport_write(I2CCON, ctrl);
+	clockport_write(&sc, I2CCON, ctrl);
 
 	RemIntServer(INTB_EXTER, int6);
 	FreeMem(int6, sizeof(struct Interrupt));
-	FreeSignal(sig_intr);
+	FreeSignal(sc.sig_intr);
 
-	printf("ISR was called %d times\n", isr_called);
+	printf("ISR was called %d times\n", sc.isr_called);
     
     return 0;
 }
 
 void
-pca9564_read_1(UBYTE address)
+pca9564_read_1(pca9564_state_t *sp, UBYTE address)
 {
 	/*assert(cur_op == OP_NOP);
 	assert(address > 1);*/
 
-	cur_op = OP_READ;
-	slave_addr = address;
-	bytes_left = 1;
+	sp->cur_op = OP_READ;
+	sp->slave_addr = address;
+	sp->bytes_left = 1;
 
 	printf("gonna send start\n");
-	pca9564_send_start();
+	pca9564_send_start(sp);
 
-	Wait(sigmask_intr);
+	Wait(sp->sigmask_intr);
 
 	Delay(10);
-	pca9564_dump_state();
+	pca9564_dump_state(sp);
 /*
 	printf("gonna send stop\n");
 	pca9564_send_stop();
 	pca9564_dump_state();
 */
 
-	slave_addr = 0;
-	cur_op = OP_NOP;
+	sp->slave_addr = 0;
+	sp->cur_op = OP_NOP;
 }
 
-void
+/*void
 pca9564_send_stop(void)
 {
 	UBYTE c;
@@ -177,77 +194,79 @@ pca9564_send_stop(void)
 	c = clockport_read(I2CCON);
 	c |= I2CCON_STO;
 	c &= (I2CCON_STA);
-	clockport_write(I2CCON, c);	/* send STOP condition */
-}
+	clockport_write(I2CCON, c);	
+}*/
 
 void
-pca9564_send_start(void) 
+pca9564_send_start(pca9564_state_t *sp) 
 {
 	UBYTE c;
 
-	c = clockport_read(I2CCON);
+	c = clockport_read(sp, I2CCON);
 	c |= I2CCON_STA|I2CCON_AA;
-	clockport_write(I2CCON, c);	/* send START condition */
+	clockport_write(sp, I2CCON, c);	/* send START condition */
 
 }
 
 void
-pca9564_dump_state(void)
+pca9564_dump_state(pca9564_state_t *sp)
 {
 	UBYTE c, s, d;
 
-	c = clockport_read(I2CCON);
-	s = clockport_read(I2CSTA);
-	d = clockport_read(I2CDAT);
+	c = clockport_read(sp, I2CCON);
+	s = clockport_read(sp, I2CSTA);
+	d = clockport_read(sp, I2CDAT);
 	printf("I2CCON: %x, I2CSTA: %x, I2CDAT: %x\n", c, s, d);
 
 }
 
 /* Interrupt service routine. */
-void
-pca9564_isr(void) 
+__amigainterrupt void
+pca9564_isr(__reg("a1") pca9564_state_t *sp) 
 {
-	UBYTE *conptr, *datptr, *staptr;
 	UBYTE v;
 
-	isr_called++; 
+	sp->in_isr = TRUE;
+	sp->isr_called++; 
 
-	conptr = cp + (I2CCON * CLOCKPORT_STRIDE);
-	datptr = cp + (I2CDAT * CLOCKPORT_STRIDE);
-	staptr = cp + (I2CSTA * CLOCKPORT_STRIDE);
-
-	if (! (*conptr & I2CCON_SI)) 
+	if (!(clockport_read(sp, I2CCON) & I2CCON_SI)) {
+		sp->in_isr = FALSE;
 		return;
+	}
 
-	switch (cur_op) {
+	switch (sp->cur_op) {
 	case OP_READ:
-		switch (*staptr) {
+		switch (clockport_read(sp, I2CSTA)) {
 		case I2CSTA_START_SENT:		/* 0x08 */
-			*datptr = (slave_addr << 1) | 1;
-			*conptr &= ~(I2CCON_SI|I2CCON_STA); /*|I2CCON_AA);*/
+			clockport_write(sp, I2CDAT, (sp->slave_addr << 1) | 1);
+			v = clockport_read(sp, I2CCON);
+			v &= ~(I2CCON_SI|I2CCON_STA); 
+			clockport_write(sp, I2CCON, v);
 			break;
 		case I2CSTA_SLAR_TX_ACK_RX:	/* 0x40 */
-			v = *conptr;
+			v = clockport_read(sp, I2CCON);
 			v &= ~(I2CCON_SI|I2CCON_AA); /*XXX: last byte */
-			*conptr = v;
+			clockport_write(sp, I2CCON, v);
 			break;
 		case I2CSTA_DATA_RX_NACK_TX:	/* 0x58 */
-			v = *conptr;
+			v = clockport_read(sp, I2CCON);
 			v &= ~(I2CCON_SI);
 			v |= (I2CCON_AA|I2CCON_STO);
-			*conptr = v;
-			Signal(MainTask, sigmask_intr);
+			clockport_write(sp, I2CCON, v);
+			Signal(sp->MainTask, sp->sigmask_intr);
 			break;
 		default:
-			*conptr = 0;
+			/* insert error handler here */
+			clockport_write(sp, I2CCON, 0);
 			break;
 		}
 		break;
 	case OP_NOP:
-		*conptr = 0;
+		/* insert error handler here */
+		clockport_write(sp, I2CCON, 0);
 		break;
 	}
 
-
+	sp->in_isr = FALSE;
 }
 
